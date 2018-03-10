37d6af6c72260d1e40abd2b837756837aae17d76
IvanPoddubny <ivan.poddubny@fanhubmedia.com>
2018-03-10 16:39:13 +0200

    Initial commit

---
+++ p0301activityresult/src/main/java/ru/startandroid/p0301activityresult/MainActivity.java
@@ -0,0 +1,36 @@
+package ru.startandroid.p0301activityresult;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    TextView tvText;
+    Button btnColor;
+    Button btnAlign;
+
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        tvText = (TextView) findViewById(R.id.tvText);
+
+        btnColor = (Button) findViewById(R.id.btnColor);
+        btnAlign = (Button) findViewById(R.id.btnAlign);
+
+        btnColor.setOnClickListener(this);
+        btnAlign.setOnClickListener(this);
+
+    }
+
+    @Override
+    public void onClick(View v) {
+        // TODO Auto-generated method stub
+
+    }
+}
