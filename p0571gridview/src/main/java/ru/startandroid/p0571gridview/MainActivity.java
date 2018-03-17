59bda4aa1f98e770c1333cbef2540c9fd7539f55
IvanPoddubny <ivan.poddubny@fanhubmedia.com>
2018-03-17 20:46:37 +0200

    lessons 50-63

---
+++ p0571gridview/src/main/java/ru/startandroid/p0571gridview/MainActivity.java
@@ -0,0 +1,35 @@
+package ru.startandroid.p0571gridview;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.GridView;
+
+public class MainActivity extends AppCompatActivity {
+
+    String[] data = {"a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k"};
+
+    GridView gvMain;
+    ArrayAdapter<String> adapter;
+
+
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        adapter = new ArrayAdapter<String>(this, R.layout.item, R.id.tvText, data);
+        gvMain = (GridView) findViewById(R.id.gvMain);
+        gvMain.setAdapter(adapter);
+        adjustGridView();
+    }
+
+
+    private void adjustGridView() {
+        gvMain.setNumColumns(GridView.AUTO_FIT);
+        gvMain.setColumnWidth(280);
+        gvMain.setVerticalSpacing(25);
+        gvMain.setHorizontalSpacing(25);
+        gvMain.setStretchMode(GridView.STRETCH_SPACING);
+    }
+}
